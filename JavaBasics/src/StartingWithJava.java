66c876758f9b06e8e855bafb23717ed8fee93b1d
Mahesh-610 <81559641+Mahesh-610@users.noreply.github.com>
2021-08-12 17:10:39 +0530

    Update StartingWithJava.java

--- JavaBasics/src/StartingWithJava.java
+++ JavaBasics/src/StartingWithJava.java
@@ -7,6 +7,7 @@ public class StartingWithJava {
 		System.out.println("From Whizdom training");
 		System.out.print("Cursor not moved to next line");
 		System.out.println(1234567890);
+		System.out.println("New line added through Git");
 
 	}
 
