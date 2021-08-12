dff52798b5b6148486ec43a4cc3a9f3ba4349d41
Mahesh-610 <mahesh.kumar.610@gmail.com>
2021-08-12 17:12:50 +0530

    Second commit

--- JavaBasics/src/StartingWithJava.java
+++ JavaBasics/src/StartingWithJava.java
@@ -7,7 +7,6 @@ public class StartingWithJava {
 		System.out.println("From Whizdom training");
 		System.out.print("Cursor not moved to next line");
 		System.out.println(1234567890);
-		System.out.println("New line added through Git");
 
 	}
 
